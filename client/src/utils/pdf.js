fd5f08684a0593c935f5d04b265de05521328f4f
Vishnu-0110 <vishnu01102005@gmail.com>
2026-06-28 21:20:43 +0530

    update

--- client/src/utils/pdf.js
+++ client/src/utils/pdf.js
@@ -103,11 +103,12 @@ const buildInvoiceHTML = (invoice, { template = false } = {}) => {
             </div>
 
             <div style="text-align: left;">
-              <div style="font-size: 23px; font-weight: 600; line-height: 1.12; margin-bottom: 4px;">${COMPANY.nameLine1} ${COMPANY.nameLine2}</div>
-              <div style="font-size: 14px; font-weight: 500;">${COMPANY.address1}</div>
-              <div style="font-size: 14px; font-weight: 500;">${COMPANY.address2}</div>
-              <div style="font-size: 14px; font-weight: 500;">${COMPANY.address3}</div>
-              <div style="font-size: 15px; font-weight: 600; margin-top: 2px;">${COMPANY.gst}</div>
+              <div style="font-size: 28px; font-weight: 700; line-height: 1.05;">${COMPANY.nameLine1}</div>
+              <div style="font-size: 28px; font-weight: 700; line-height: 1.05; margin-bottom: 14px;">${COMPANY.nameLine2}</div>
+              <div style="font-size: 18px; font-weight: 600; line-height: 1.18;">${COMPANY.address1}</div>
+              <div style="font-size: 18px; font-weight: 600; line-height: 1.18;">${COMPANY.address2}</div>
+              <div style="font-size: 18px; font-weight: 600; line-height: 1.18;">${COMPANY.address3}</div>
+              <div style="font-size: 18px; font-weight: 700; margin-top: 2px;">${COMPANY.gst}</div>
             </div>
           </div>
 
